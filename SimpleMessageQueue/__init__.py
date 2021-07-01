fe2f94eada9a9bc388627102479a31c22f018b73
Lawrence Yu <lawy888@gmail.com>
2021-06-30 20:09:26 -0700

    change to blocking server

--- SimpleMessageQueue/__init__.py
+++ SimpleMessageQueue/__init__.py
@@ -13,12 +13,27 @@ from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
 from socketserver import ThreadingMixIn
 from concurrent.futures import ThreadPoolExecutor
+import http
 
 
 # --------------------------------------------------
 #    Extended SimpleXMLRPCServer
 # --------------------------------------------------
 class SimpleXMLRPCServerEx(ThreadingMixIn, SimpleXMLRPCServer):
+    def __init__(self, addr, *args, **kwargs):
+        super().__init__(addr, *args, **kwargs)
+        self._shutdown = False
+
+    def shutdown(self):
+        self._shutdown = True
+
+    def serve_forever(self):
+        self.timeout = 0.1
+        while not self._shutdown:
+            self.handle_request()
+
+
+class SimpleXMLRPCServerBlocking(SimpleXMLRPCServer):
     def __init__(self, addr, *args, **kwargs):
         super().__init__(addr, *args, **kwargs)
         self._shutdown = False
@@ -40,12 +55,16 @@ class SMQServer():
         # init
         self._clients = {}
 
+    def nop(self):
+        return
+
     def publish_message(self, client_info, msg, msg_data, direct_smq_uid=None):
         try:
             # sanity check
             cuid = client_info['smq_uid']
             if cuid not in self._clients:
                 logging.warning('Received message from client %s that is not registered' % cuid)
+            logging.info(f'publishing message from {cuid}')
 
             # verify the message is in the pub list
             if msg not in self._clients[cuid]['pub_list']:
@@ -68,13 +87,18 @@ class SMQServer():
                     logging.info(f'Forwarding message {msg} to {c}')
                     try:
                         ci['client_rpc_server'].receive_message(cuid, msg, msg_data)
-                    except ConnectionRefusedError:
+                        logging.info(f'Forwarding message Finished {msg} to {c}')
+#                    except (ConnectionRefusedError, http.client.CannotSendRequest):
+                    except (ConnectionRefusedError):
                         logging.info(f'Removing client {c} because connection refused')
                         # this may fail if another thread deletes at the same time
                         try:
                             del self._clients[c]
                         except Exception:
                             pass
+                    except Exception as e:
+                        logging.error('***************' + str(e))
+                        logging.error('***************' + str(type(e)))
         except Exception as e:
             logging.error(traceback.format_exc())
             raise(e)
@@ -114,7 +138,7 @@ class SMQServer():
         kwargs['allow_none'] = kwargs.get('allow_none', True)
         kwargs['logRequests'] = kwargs.get('logRequests', False)
         server_impl = SMQServer()
-        server = SimpleXMLRPCServerEx(addr, *args, **kwargs)
+        server = SimpleXMLRPCServerBlocking(addr, *args, **kwargs)
         server.register_instance(server_impl)
         server.serve_forever()
 
@@ -131,61 +155,92 @@ class SMQClient():
     def __init__(self):
         # init
         self._client_info = None
-        self._executor = ThreadPoolExecutor(max_workers=4)
+        self._executor = ThreadPoolExecutor(max_workers=64)
         self._local_rpc_server = None
         self._local_rpc_server_thread = None
         self._message_handler = None
         self._message_queue = deque()
         self._smq_server = None
+        self._restart_info = None
+        self._connected = False
+
+    def connected(self):
+        if self._connected:
+            try:
+                self._smq_server.nop()
+            except Exception as e:
+                print(e)
+                self._connected = False
+        return self._connected
 
     def start_client(self, smq_server, client_name, pub_list_str, sub_list_str, message_handler=None):
         # sanity check
-        if self._local_rpc_server:
-            raise Exception('Client already started')
+        try:
+            if self._local_rpc_server:
+                raise Exception('Client already started')
 
-        logging.info('Starting Client...')
+            self._restart_info = {'smq_server': smq_server, 'client_name': client_name,
+                                  'pub_list_str': pub_list_str, 'sub_list_str': sub_list_str,
+                                  'message_handler': message_handler}
 
-        # set start flag
-        self._message_handler = message_handler
+            logging.info('Starting Client...')
 
-        # init the rpc to the main smq server
-        self._smq_server = xmlrpc.client.ServerProxy('http://' + smq_server, allow_none=True)
+            self._connected = True
 
-        # start XML RPC server on new thread
-        self._local_rpc_server = SimpleXMLRPCServerEx(('', 0), allow_none=True, logRequests=False)
-        self._local_rpc_server.register_function(self.receive_message)
-        self._local_rpc_server_thread = threading.Thread(target=lambda: self._local_rpc_server.serve_forever(),
-                                                         daemon=True)
-        self._local_rpc_server_thread.start()
+            # set start flag
+            self._message_handler = message_handler
 
-        # register this client with the main smq server
-        client_rpc_url = socket.gethostname() + ':' + str(self._local_rpc_server.server_address[1])
-        self._client_info = {'client_name': client_name, 'pub_list_str': pub_list_str, 'sub_list_str': sub_list_str,
-                             'client_hostname': socket.gethostname(), 'client_pid': os.getpid(),
-                             'client_rpc_url': client_rpc_url, 'smq_uid': uuid.uuid4().hex}
-        logging.info(self._client_info)
-        self._smq_server.register_client(self._client_info)
+            # init the rpc to the main smq server
+            self._smq_server = xmlrpc.client.ServerProxy('http://' + smq_server, allow_none=True)
+
+            # start XML RPC server on new thread
+            self._local_rpc_server = SimpleXMLRPCServerBlocking(('', 0), allow_none=True, logRequests=False)
+            self._local_rpc_server.register_function(self.receive_message)
+            self._local_rpc_server_thread = threading.Thread(target=lambda: self._local_rpc_server.serve_forever(),
+                                                             daemon=True)
+            self._local_rpc_server_thread.start()
+
+            # register this client with the main smq server
+            client_rpc_url = socket.gethostname() + ':' + str(self._local_rpc_server.server_address[1])
+            self._client_info = {'client_name': client_name, 'pub_list_str': pub_list_str, 'sub_list_str': sub_list_str,
+                                 'client_hostname': socket.gethostname(), 'client_pid': os.getpid(),
+                                 'client_rpc_url': client_rpc_url, 'smq_uid': uuid.uuid4().hex}
+            logging.info(self._client_info)
+            self._smq_server.register_client(self._client_info)
+        except Exception as e:
+            self._connected = False
+            raise(e)
 
     def publish_message(self, msg, msg_data):
         # publish message to main smq server
-        logging.info(f'Publishing Message "{msg}"')
-        self._smq_server.publish_message(self._client_info, msg, msg_data)
+        try:
+            logging.info(f'Publishing Message "{msg}"')
+            self._smq_server.publish_message(self._client_info, msg, msg_data)
+        except Exception as e:
+            self._connected = False
+            raise(e)
 
     def send_direct_message(self, smq_uid, msg, msg_data):
         # send a direct message to a specific client
-        logging.info(f'Sending Direct Message "{msg}" to {smq_uid}')
-        self._smq_server.publish_message(self._client_info, msg, msg_data, smq_uid)
+        try:
+            logging.info(f'Sending Direct Message "{msg}" to {smq_uid}')
+            self._smq_server.publish_message(self._client_info, msg, msg_data, smq_uid)
+        except Exception as e:
+            self._connected = False
+            raise(e)
 
     def receive_message(self, sender_smq_uid, msg, msg_data):
         # receive message from main smq server
-        logging.info(f'Received Message "{msg}"')
-#         if self._client_info['smq_uid'] != smq_uid:
-#             logging.warning('Mismatched client UID')
-#             return
-        kwargs = {'smq_uid': sender_smq_uid, 'msg': msg, 'msg_data': msg_data}
-        self._message_queue.append(kwargs)
-        if self._message_handler:
-            self._executor.submit(self._message_handler, **kwargs)
+        try:
+            logging.info(f'Received Message "{msg}"')
+            kwargs = {'smq_uid': sender_smq_uid, 'msg': msg, 'msg_data': msg_data}
+            self._message_queue.append(kwargs)
+            if self._message_handler:
+                self._executor.submit(self._message_handler, **kwargs)
+            logging.info(f'Received Message Finished "{msg}"')
+        except Exception as e:
+            self._connected = False
+            raise(e)
 
     def get_message(self):
         if self._message_queue:
@@ -194,14 +249,18 @@ class SMQClient():
     def shutdown(self):
         """ shutdown the SMQ client """
         # sanity check
-        logging.info('smq client shutdown')
-        if not self._started:
-            raise Exception('Client not started')
-        self._smq_server.unregister_client(self._client_info)
-        self._local_rpc_server.shutdown()
-        self._local_rpc_server_thread.join()
-        self._local_rpc_server_thread = None
-        self._client_info = None
-        self._local_rpc_server = None
-        self._smq_server = None
-        self._started = False
+        try:
+            logging.info(f"smq client shutdown {self._client_info['smq_uid']}")
+            if not self._connected:
+                raise Exception('Client not connected')
+            self._smq_server.unregister_client(self._client_info)
+            self._local_rpc_server.shutdown()
+            self._local_rpc_server_thread.join()
+            self._local_rpc_server_thread = None
+            self._client_info = None
+            self._local_rpc_server = None
+            self._smq_server = None
+            self._connected = False
+        except Exception as e:
+            self._connected = False
+            raise(e)
