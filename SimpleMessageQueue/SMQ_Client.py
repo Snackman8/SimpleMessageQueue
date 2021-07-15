ab1c7115c36b6fbc4fc1f58fae29e9c475837a98
Lawrence Yu <lawy888@gmail.com>
2021-07-14 23:50:53 -0700

    big refactor

---
+++ SimpleMessageQueue/SMQ_Client.py
@@ -0,0 +1,218 @@
+# --------------------------------------------------
+#    Imports
+# --------------------------------------------------
+import logging
+import threading
+import time
+import xmlrpc.client
+import uuid
+
+
+# --------------------------------------------------
+#    Class
+# --------------------------------------------------
+class SMQ_Client():
+    def __init__(self, smq_server_url, client_name, client_id, classifications, pub_list, sub_list, polling_interval=1):
+        """ init """
+        self._classification = classifications
+        self._client_id = client_id
+        self._client_name = client_name
+        self._incoming_queue = None
+        self._message_handlers = {}
+        self._outgoing_queue = None
+        self._polling_interval = polling_interval
+        self._pub_list = pub_list
+        self._response_callbacks = {}
+        self._shutdown = False
+        self._smq_server_url = smq_server_url
+        self._started = False
+        self._sub_list = sub_list
+
+    def __del__(self):
+        """ unregister client when this instance is destroyed """
+        self.stop()
+
+    def _tw_process_message(self, msg):
+        """ process an incoming message by delegating to the handler and returning a response
+
+            Args:
+                msg - message to process
+        """
+        logging.info(f'received msg - {msg["msg_type"]}')
+        # handle if a response
+        if 'response_payload' in msg:
+            if msg['msg_uuid'] in self._response_callbacks:
+                handler = self._response_callbacks[msg['msg_uuid']]
+                del self._response_callbacks[msg['msg_uuid']]
+                if handler is not None:
+                    handler(msg)
+            return
+
+        # handle a normal message
+        handler = self._message_handlers.get(msg['msg_type'], None)
+        if handler:
+            retval = handler(msg, self)
+        else:
+            retval = None
+        self.send_message(SMQ_Client.construct_response_msg(msg, retval))
+
+    def _tw_pump_messages(self):
+        """ thread worker which will create new threads to handle each incoming message """
+        # flag for reconnect needed
+        need_reconnect = False
+        
+        while not self._shutdown:
+            try:
+                if need_reconnect:
+                    logging.info(f'Reconnecting {self._client_name} {self._client_id} {self._classification} ' +
+                                 f'{self._pub_list} {self._sub_list}')
+                    with xmlrpc.client.ServerProxy(self._smq_server_url, allow_none=True) as sp:
+                        sp.register_client(self._client_name, self._client_id, self._classification, self._pub_list,
+                                           self._sub_list)
+                    need_reconnect = False
+                    time.sleep(1)
+
+                with xmlrpc.client.ServerProxy(self._smq_server_url, allow_none=True) as sp:
+                    msg = sp.pop_message(self._client_id)
+                if msg is not None:
+                    t = threading.Thread(target=self._tw_process_message, args=[msg], daemon=True)
+                    t.start()
+                else:
+                    time.sleep(self._polling_interval)
+            except ConnectionRefusedError:
+                logging.warning(f'SMQ Server at {self._smq_server_url} is refusing connection')
+                time.sleep(5)
+                need_reconnect = True
+            except Exception as e:
+                logging.exception(e)
+                time.sleep(5)
+
+    def add_message_handler(self, msg_type, handler):
+        """ add a handler function that will be called when a message of msg_type is received
+
+            Args:
+                msg_type - type of message to bind this handler to.  If a handler already exists for this message type
+                           it will be overwritten by the new handler.  There can only be one handler for a message type
+                handler - handler function for the msg_type.  Prototype for the handler is f(msg)
+        """
+        self._message_handlers[msg_type] = handler
+
+    def construct_msg(self, msg_type, target_id, payload, msg_uuid=None, sender_id=None):
+        """ construct a message for the Simple Message Queue
+
+            Args:
+                msg_type - type of message, i.e. 'reload_config'
+                target_id - client id of the recipient of this message, use '*' for a broadcast to everyone
+                payload - dictionary containing the data for this message, the structure of the dict is unique to each
+                          msg_type
+                msg_uuid - unique id of this message, no two messages can ever share the same uuid.  If none, a uuid
+                           will be generated
+                sender_id - client id of the sender of this message.  If none, the client_id of this SMQ_Client will be
+                            used
+
+            Returns:
+                dict containing the message
+        """
+        if msg_uuid is None:
+            msg_uuid = uuid.uuid4().hex
+        if sender_id is None:
+            sender_id = self._client_id
+
+        return {'msg_type': msg_type, 'msg_uuid': msg_uuid, 'target_id': target_id, 'sender_id': sender_id,
+                'payload': payload}
+
+    @classmethod
+    def construct_response_msg(cls, og_msg, response_payload):
+        """ construct a response to a message for the Simple Message Queue
+
+            Args:
+                og_msg - original message being responded to
+                response_payload - dictionary containing the response data to the original message
+
+            Returns:
+                dict containing the message
+        """
+        return {'msg_type': og_msg['msg_type'] + '_response', 'msg_uuid': og_msg['msg_uuid'],
+                'target_id': og_msg['sender_id'], 'sender_id': og_msg['target_id'], 'payload': og_msg['payload'],
+                'response_payload': response_payload}
+
+    def get_info_for_all_clients(self):
+        """ Proxy to the SMQ Server call get_info_for_all_clients.  See the SQM Server documentation for more details
+
+            The SMQ Client does not need to be started to call this function
+        """
+        with xmlrpc.client.ServerProxy(self._smq_server_url, allow_none=True) as sp:
+            client_info = sp.get_info_for_all_clients()
+        logging.info(f'get_info_for_all_clients')
+        for k, v in client_info.items():
+            logging.info(f'    {k}: {v}')
+        return client_info
+
+    def remove_message_handler(self, msg_type):
+        """ remove the message handler for a message type
+
+            Args:
+                msg_type - message type to remove the handler for
+
+            Returns:
+                the handler that was assigned to the message type before removal
+        """
+        return self._message_handlers.pop(msg_type, None)
+
+    def send_message(self, msg, response_callback=None, wait=0):
+        """ send a message constructed by construct_msg to its recipient
+
+            Args:
+                msg - message to send
+                response_callback - handler that will be called when response is received from the recipient after
+                                    processing the message.  Must be None if wait is non-zero.
+                wait - if 0, no response handling will happen.  Otherwise the number of seconds to wait for the response
+                       before throwing a timeout exception
+
+            Returns:
+                if wait is 0, then None.  Otherwise the response payload will be returned
+        """
+        # blocking response handler if wait != 0
+        response_msg, response_returned = None, False
+
+        def response_handler(msg):
+            nonlocal response_msg, response_returned
+            response_msg, response_returned = msg, True
+
+        if wait != 0:
+            response_callback = response_handler
+
+        # log and setup the response_callback
+        logging.info(f'Sending {msg["msg_uuid"]} {msg["msg_type"]} {msg["payload"]} ' +
+                     f'from {msg["sender_id"]} to {msg["target_id"]}')
+        self._response_callbacks[msg['msg_uuid']] = response_callback
+
+        # send the message
+        with xmlrpc.client.ServerProxy(self._smq_server_url, allow_none=True) as sp:
+            sp.send_message(msg)
+
+        # handle wait != 0
+        if wait != 0:
+            start_time = time.time()
+            while not response_returned:
+                if time.time() - start_time > wait:
+                    raise TimeoutError()
+                time.sleep(0.01)
+            return response_msg['response_payload']
+
+    def start(self):
+        """ start the SMQ Client """
+        logging.info(f'Starting {self._client_name} {self._client_id} {self._classification} {self._pub_list} ' +
+                     f'{self._sub_list}')
+        with xmlrpc.client.ServerProxy(self._smq_server_url, allow_none=True) as sp:
+            sp.register_client(self._client_name, self._client_id, self._classification, self._pub_list, self._sub_list)
+        threading.Thread(target=self._tw_pump_messages, daemon=True).start()
+        self._started = True
+
+    def stop(self, force=False):
+        """ stop the SMQ Client """
+        if self._started or force:
+            logging.info(f'Stopping {self._client_name} {self._client_id}')
+            self._shutdown = True
+            with xmlrpc.client.ServerProxy(self._smq_server_url, allow_none=True) as sp:
+                sp.unregister_client(self._client_id)
---
+++ SimpleMessageQueue/SMQ_Server.py
@@ -0,0 +1,180 @@
+# --------------------------------------------------
+#    Imports
+# --------------------------------------------------
+import argparse
+import logging
+import multiprocessing
+import queue
+import threading
+from xmlrpc.server import SimpleXMLRPCServer
+
+
+# --------------------------------------------------
+#    Class
+# --------------------------------------------------
+class SMQ_Server():
+    def __init__(self, hostname, port):
+        """ init """
+        self._dispatch_thread = None
+        self._hostname = hostname
+        self._mp_manager = multiprocessing.Manager()
+        self._port = port
+        self._registered_clients = {}
+        self._rpc_server = None
+        self._shutdown = False
+
+        # create our incoming queue
+        self._incoming_queue = self._mp_manager.Queue()
+
+    def _tw_dispatch(self):
+        while not self._shutdown:
+            try:
+                msg = self._incoming_queue.get(block=True, timeout=0.1)
+                if msg['target_id'] == '*':
+                    targets = self._registered_clients.keys()
+                else:
+                    targets = [msg['target_id']]
+                for t in targets:
+                    if t in self._registered_clients:
+                        logging.info(f'Dispatching {msg["msg_uuid"]} {msg["msg_type"]} {msg["payload"]} ' +
+                                     f'from {msg["sender_id"]} to {t}')
+                        self._registered_clients[t]['incoming_queue'].put(msg, block=False)
+                        logging.info(f'Dispatched')
+            except queue.Empty:
+                pass
+            except Exception as e:
+                logging.exception(e)
+
+    def get_info_for_all_clients(self):
+        """
+            return information about all registered clients
+
+            Returns:
+                {client_id: {'client_name': client_name, 'classifications': classifications, 'pub_list': pub_list,
+                             'sub_list': sub_list}
+                 ...}
+        """
+        client_info = {}
+        for k, v in self._registered_clients.items():
+            client_info[k] = {kk: v[kk] for kk in ('client_name', 'classifications', 'pub_list', 'sub_list')}
+        return client_info
+
+    def pop_message(self, client_id):
+        """ pop a message from the incoming_queue for the client_id
+
+            Args:
+                client_id - client_id of the queue to check
+
+            Returns:
+                msg if available or None if no message is available
+        """
+        try:
+            msg = self._registered_clients[client_id]['incoming_queue'].get(block=False)
+            logging.info(f'Message popped {client_id} {msg["msg_type"]}')
+            return msg
+        except queue.Empty:
+            return None
+        except Exception as e:
+            logging.exception(e)
+
+    def register_client(self, client_name, client_id, classifications, pub_list, sub_list):
+        """
+            register a client with the SMQ Server
+
+            Args:
+                client_name - displayable name of the client
+                client_id - unique ID of this client
+                classifications - list of items classifying this client, i.e. ['FlowController', 'test.cfg']
+                pub_list - list of messages published by this client, i.e. ['ping', 'reload']
+                sub_list - list of messages subscribed by this client, i.e. ['ping_response', 'reload_response']
+        """
+        logging.info(f'Registering Client {client_name} {client_id} {classifications} {pub_list} {sub_list}')
+
+        client_incoming_queue = self._mp_manager.Queue()
+        self._registered_clients[client_id] = {'client_name': client_name, 'classifications': classifications,
+                                               'pub_list': pub_list, 'sub_list': sub_list,
+                                               'incoming_queue': client_incoming_queue}
+
+    def shutdown(self):
+        """ called by rpc to shutdown this SMQ Server """
+        logging.info('Shutting down server')
+
+        # shutdown the RPC
+        t = threading.Thread(target=lambda: self._rpc_server.shutdown())
+        t.start()
+        self._shutdown = True
+
+    def start(self):
+        """ start the RPC server for the SMQ_Server using the port passed in in __init__
+
+            register functions for get_info_for_all_clients
+                                   pop_message
+                                   register_client
+                                   send_message
+                                   shutdown
+                                   unregister_client
+        """
+        logging.info(f'Starting SMQ Server Dispatch Thread')
+        self._dispatch_thread = threading.Thread(target=self._tw_dispatch)
+        self._dispatch_thread.start()
+
+        logging.info(f'Starting SMQ Server RPC on port {self._port}')
+        self._rpc_server = SimpleXMLRPCServer(('', self._port), allow_none=True, logRequests=False)
+        self._rpc_server.register_function(self.get_info_for_all_clients)
+        self._rpc_server.register_function(self.pop_message)
+        self._rpc_server.register_function(self.register_client)
+        self._rpc_server.register_function(lambda msg: self._incoming_queue.put(msg, block=False), name='send_message')
+        self._rpc_server.register_function(self.shutdown)
+        self._rpc_server.register_function(self.unregister_client)
+        self._rpc_server.serve_forever()
+
+    def unregister_client(self, client_id):
+        """
+            unregisters a client with the SMQ Server
+
+            Args:
+                client_id - unique ID of the client to unregister
+        """
+        logging.info(f'Unregistering Client {client_id}')
+        if client_id in self._registered_clients:
+            logging.info(f'Deleting Client {client_id}')
+            del self._registered_clients[client_id]
+
+
+# --------------------------------------------------
+#    Main
+# --------------------------------------------------
+def run(args):
+    """ run """
+    # start the server
+    if args['start_server']:
+        server = SMQ_Server(args['hostname'], args['port'])
+        server.start()
+
+
+if __name__ == "__main__":
+    # setup logging
+    logging.getLogger().setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(threadName)s %(message)s'))
+    logging.getLogger().addHandler(console_handler)
+
+    try:
+        # parse the arguments
+        parser = argparse.ArgumentParser(description='Simple Message Queue Server')
+        parser.add_argument('--hostname', required=True, help='hostname to run SMQ Server on')
+        parser.add_argument('--port', type=int, required=True, help='port to run SMQ Server on')
+        parser.add_argument('--start_server', action='store_true', help='start the SMQ Server')
+        parser.add_argument('--logging_level', default='INFO', help='logging level')
+        args = parser.parse_args()
+
+        # setup logging level
+        logging.info('Starting ', vars(args))
+        logging.getLogger().setLevel(getattr(logging, args.logging_level))
+
+        # run
+        run(vars(args))
+    except Exception as e:
+        logging.exception('Exception')
+        raise(e)
